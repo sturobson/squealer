202159803a6d9c2571002200cb151faefa63e22b
Stu Robson <stu@alwaystwisted.com>
2014-11-23 21:11:06 +0000

    adds copy to move images for assets to dev

--- grunt/copy.js
+++ grunt/copy.js
@@ -3,5 +3,10 @@ module.exports = {
     files: [
       {expand: true, flatten: true, src: ['assets/js/vendor/*'], dest: 'dev/js/vendor/', filter: 'isFile'}
     ],
+  },
+  img: {
+    files: [
+      {expand: true, flatten: true, src: ['assets/images/**/*'], dest: 'dev/images', filter: 'isFile'}
+    ],
   }
 };
