6b715fd2167be8e87e8ee457ee318d10bc5c115c
Stu Robson <stu@alwaystwisted.com>
2014-11-23 20:01:51 +0000

    adds css pretty

---
+++ grunt/csspretty.js
@@ -0,0 +1,22 @@
+module.exports = {
+  dist: {
+    options: {
+      decl: {
+        before: '\n',
+        between: ':',
+      },
+      rule: {
+        before: '\n\n',
+        between: '',
+        after: '\n',
+      },
+      atRule: {
+        before: '\n\n',
+        between: '',
+        after: '\n\n',
+      },
+      selectors: 'separateline',
+    },
+    src: 'dev/css/oldie.css',
+  }
+};
