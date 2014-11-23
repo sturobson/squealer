4c7dd32b58b107b27c1b7f7636750507383663c6
Stu Robson <stu@alwaystwisted.com>
2014-11-23 21:10:08 +0000

    adds imageoptim plugin

---
+++ grunt/imageoptim.js
@@ -0,0 +1,18 @@
+module.exports = {
+  allPngs: {
+    options: {
+      imageAlpha: true,
+      jpegMini: false,
+      quitAfter: true
+    },
+    src: ['assets/images/']
+  },
+  allJpgs: {
+    options: {
+      imageAlpha: false,
+      jpegMini: true,
+      quitAfter: true
+    },
+    src: ['assets/images/']
+  }
+};
