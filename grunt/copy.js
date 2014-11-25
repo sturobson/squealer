c8b003bf39e704ff93c403b092625d7309cc108d
Stu Robson <stu@alwaystwisted.com>
2014-11-25 21:58:41 +0000

    updates copy.js to reflect additional assets folder in dev

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,12 +1,12 @@
 module.exports = {
   copy: {
     files: [
-      {expand: true, flatten: true, src: ['assets/js/vendor/*'], dest: 'dev/js/vendor/', filter: 'isFile'}
+      {expand: true, flatten: true, src: ['assets/js/vendor/*'], dest: 'dev/assets/js/vendor/', filter: 'isFile'}
     ],
   },
   img: {
     files: [
-      {expand: true, flatten: true, src: ['assets/images/**/*'], dest: 'dev/images', filter: 'isFile'}
+      {expand: true, flatten: true, src: ['assets/images/**/*'], dest: 'dev/assets/images/', filter: 'isFile'}
     ],
   }
 };
