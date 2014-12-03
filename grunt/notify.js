c5000826582507832180e6f6d08b8f123cd2bfc3
Stu Robson <stu@alwaystwisted.com>
2014-12-03 21:23:40 +0000

    adds grunt-notify

---
+++ grunt/notify.js
@@ -0,0 +1,8 @@
+module.exports = {
+  options: {
+    enabled: true,
+    max_jshint_notifications: 5, // maximum number of notifications from jshint output
+    success: false, // whether successful grunt executions should be notified automatically
+    duration: 2 // the duration of notification in seconds, for `notify-send only
+  }
+};
