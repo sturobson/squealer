070985f9606560e54f7b300e80df8de8606c074b
Stu Robson <stu@alwaystwisted.com>
2014-12-04 23:09:45 +0000

    fixed spelling mistake in uglify plugin code

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,8 +1,8 @@
 module.exports = {
   my_target: {
     files: {
-      'dev/asssets/js/plugins.js': 'assets/js/plugins.js',
-      'dev/asssets/js/scripts.js': 'assets/js/scripts.js'
+      'dev/assets/js/plugins.js': 'assets/js/plugins.js',
+      'dev/assets/js/scripts.js': 'assets/js/scripts.js'
     }
   }
 };
