5bf3a482ae182852b05f3cf4719d4a31c2fe20a4
Stu Robson <stu@alwaystwisted.com>
2014-12-16 21:12:59 +0000

    adds image-imbed and load_perf

---
+++ grunt/image-embed.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dist: {
+    src: [ "css/styles.css" ],
+      dest: "css/output.css",
+      options: {      
+          deleteAfterEncoding : false
+      } 
+  }
+}
---
+++ grunt/load_perf.js
@@ -0,0 +1,13 @@
+module.exports = {
+    localPort: 3000,
+    url: 'localhost/shellideas360',
+    remotePort: 8080,
+    networkProfile: '3G',
+    cdn: true,
+    output: 'perf-report/',
+    animation: 'my-test.gif',
+    fps: 1,
+    viewportWidth: 1280,
+    viewportHeight: 768,
+    limit: 100
+};  
